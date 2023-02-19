05fceb0d61dc8e6df0b3070f0ca8bbf1ef98cb90
huangxiaochan <huangxiaochan1991@163.com>
2023-02-19 16:33:25 +0800

    deploy

---
+++ precache-manifest.641f8250a5d38713347aa6629b9c0703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b228b65bcb9dbb2c55d1108c3bd8bb85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f42219d9192b1770b38",
+    "url": "/static/css/main.47f2af02.chunk.css"
+  },
+  {
+    "revision": "44388ae8b5a7dd43a397",
+    "url": "/static/js/2.6e975100.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/static/js/2.6e975100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f42219d9192b1770b38",
+    "url": "/static/js/main.2aea14b7.chunk.js"
+  },
+  {
+    "revision": "538e147ae0acac02142c",
+    "url": "/static/js/runtime-main.7cdda5c5.js"
+  }
+]);
