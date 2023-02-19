98bc1fc3d3cec352ab4a8b55e4f147021698eef5
huangxiaochan <huangxiaochan1991@163.com>
2023-02-19 17:12:59 +0800

    deploy

---
+++ precache-manifest.6b0bc875d0809e6707469d11c86a2a05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4419dc0b573f03f27ba44998c0b7a363",
+    "url": "/bookeeping-react/index.html"
+  },
+  {
+    "revision": "617362344e3e792dc55a",
+    "url": "/bookeeping-react/static/css/main.47f2af02.chunk.css"
+  },
+  {
+    "revision": "c8a710980757112ac1c6",
+    "url": "/bookeeping-react/static/js/2.bfd0f557.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/bookeeping-react/static/js/2.bfd0f557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617362344e3e792dc55a",
+    "url": "/bookeeping-react/static/js/main.71f8b7c7.chunk.js"
+  },
+  {
+    "revision": "e9f1704a634f4f42429b",
+    "url": "/bookeeping-react/static/js/runtime-main.7d00fc42.js"
+  }
+]);
